8c1d63e69de2446b75df014b9b570a1ece55cc29
Santosh Karki <santosh.karki044@gmail.com>
2019-01-15 09:35:06 -0600

    service registry and discovery using Eureka Server

---
+++ spring-eureka-client-school-service/src/main/java/com/example/mac/practise/springeurekaclientschoolservice/SpringEurekaClientSchoolServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.mac.practise.springeurekaclientschoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringEurekaClientSchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaClientSchoolServiceApplication.class, args);
+	}
+
+}
+
